ba4bb3cc8347049a88adc98f2f4bb7defd62a93c
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-12-24 03:17:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/Image-Compressor/',
+	base: '/Compresor-Imagenes/',
 	plugins: [
 		vue(),
 	],
