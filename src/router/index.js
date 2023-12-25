04a9a3b92cf1ef5b87fd7e6c0af35b90533d6f83
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-12-25 21:40:27 +0100

    Small improvement

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
-      path: "/:locale?",
+      path: "/Image-Compressor/:locale?",
       component: RouterView,
       beforeEnter: Tr.routeMiddleware,
-      children: [
-      ]
     }
   ]
 })
