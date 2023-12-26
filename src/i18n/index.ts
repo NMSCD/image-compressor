34ad6c8fff1899479b3fd3451ed4243b3011c47c
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-12-26 01:01:03 +0100

    Using en-EN and es-ES for better implementation with browsers

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,13 @@
 import { createI18n } from 'vue-i18n';
-import Español from './es';
-import English from './en';
+import Español from './es-ES';
+import English from './en-EN';
 
 const messages = {
 	Español,
 	English,
 };
 
-const userLang = navigator.language.replace('es', 'Español').replace('en', 'English');
+const userLang = navigator.language.replace('es-ES', 'Español').replace('en-EN', 'English');
 const localStorageLang = localStorage.getItem('lang');
 export const preferredLang = localStorageLang ?? (Object.keys(messages).includes(userLang) ? userLang : 'English');
 
