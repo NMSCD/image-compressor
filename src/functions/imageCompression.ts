585f1c6ef27ca670c9f496aeab2350d684fa9c31
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-31 17:24:16 +0200

    fix file extension

--- src/functions/imageCompression.ts
+++ src/functions/imageCompression.ts
@@ -4,6 +4,7 @@ import { maxSize } from '@/variables/constants';
 export async function compressFile(file: File, quality: number = 1): Promise<File> {
   if (file.size < maxSize) return file; // if below 10 MB, don't do anything
   const type = imageTypes.JPEG;
+  const newFileExtension = type.split('/').at(-1);
   const res = await compressImage(file, {
     quality,
     type,
@@ -11,6 +12,6 @@ export async function compressFile(file: File, quality: number = 1): Promise<Fil
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
   if (res.size > maxSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
-  const newFileName = `${fileName}-min.${type}`;
+  const newFileName = `${fileName}-min.${newFileExtension}`;
   return new File([res], newFileName, { type });
 }
